7c451cebaea01feb4aea5051388997429810bcc4
Greg <sogmeadmug@gmail.com>
2025-02-06 14:59:57 +0300

    второй вариант для №2 и решение №3

--- lambda_list_comprehension.py
+++ lambda_list_comprehension.py
@@ -13,3 +13,17 @@ numbers_inp = input("Введите числа через пробел: ")
 numbers = list(map(try_int, numbers_inp.split()))
 
 print(numbers)
+
+numbers2 = [int(number) if number.isdigit() else None for number in numbers_inp.split()]
+print(numbers2)
+
+# 3. Используйте `filter`, чтобы создать словарь, содержащий исходные `id` и другие ключи, но только для тех фильмов, `id` которых присутствуют в списке, полученном на предыдущем шаге.
+
+
+#Противоречивое задание, как-будто подразумевался список словарей, включающих id и все остальные ключи.
+#
+
+
+filtered_dict = dict(filter(lambda movies: isinstance(movies[0], int) and movies[0] in numbers, full_dict.items()))
+print(filtered_dict)
+
